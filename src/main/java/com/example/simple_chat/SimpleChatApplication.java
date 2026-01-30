19e51779dfde8d6560d342ca6325bd06d572e613
fantadogxyz-lgtm <fantadogxyz@gmail.com>
2026-01-31 00:56:40 +0300

    tried to add a simple curl filter for fun

--- src/main/java/com/example/simple_chat/SimpleChatApplication.java
+++ src/main/java/com/example/simple_chat/SimpleChatApplication.java
@@ -1,7 +1,14 @@
 package com.example.simple_chat;
 
+import com.example.simple_chat.security.filter.DeniedClientFilter;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.session.DisableEncodeUrlFilter;
 
 @SpringBootApplication
 public class SimpleChatApplication {
@@ -10,4 +17,11 @@ public class SimpleChatApplication {
 		SpringApplication.run(SimpleChatApplication.class, args);
 	}
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .addFilterBefore(new DeniedClientFilter(), DisableEncodeUrlFilter.class)
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
 }
